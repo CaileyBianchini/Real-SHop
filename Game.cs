52320b2dc6600dd83055dd629512c0066f346844
Cailey Bianchini <cailey.m.bianchini@gmail.com>
2020-09-21 16:54:21 -0500

    dear mother of god

--- Game.cs
+++ Game.cs
@@ -4,12 +4,100 @@ using System.Text;
 
 namespace HelloWorld
 {
+
+
+    struct Items
+    {
+        public int itemBoost;
+        public string itemName;
+        public int itemPrice;
+    }
+
     class Game
     {
+
+        private Items _longSword;
+        private Items _sheild;
+        private Items _mace;
+        private Items _BowAndArrow;
+
+        private Items _fireGem;
+
+        private Items _poisonPotion;
+        private Items _fearPotion;
+        private Items _fartPotion;
+
+        private Items _money;
+
+        private Player _player;
+
+        public void InitializeItems()
+        {
+            _longSword.itemBoost = 15;
+            _longSword.itemName = "Long Sword";
+            _longSword.itemPrice = 2;
+
+            _sheild.itemBoost = 10;
+            _sheild.itemName = "Sheild";
+            _sheild.itemPrice = 1;
+
+
+            _mace.itemBoost = 17;
+            _mace.itemName = "Mace";
+            _mace.itemPrice = 4;
+
+            _BowAndArrow.itemBoost = 9;
+            _BowAndArrow.itemName = "Bow and Arrow";
+            _BowAndArrow.itemPrice = 1;
+
+            //items
+            _money.itemPrice = 10;
+
+            //Gem
+            _fireGem.itemPrice = 1;
+            _fireGem.itemName = "Fire Gem";
+            _fireGem.itemBoost = 10;
+
+            //Potions
+            _poisonPotion.itemPrice = 2;
+            _poisonPotion.itemName = "Poison Potion";
+            _poisonPotion.itemBoost = 20;
+
+            _fearPotion.itemPrice = 2;
+            _fearPotion.itemName = "Fear Potion";
+            _fearPotion.itemBoost = 10;
+
+            _fartPotion.itemPrice = 5;
+            _fartPotion.itemName = "Fart Potion";
+            _fartPotion.itemBoost = 100;
+
+        }
+
+
+        public void PrintInventory()
+        {
+            foreach(Items item in _player.GetInventory())
+            {
+                //Console.WriteLine(_inventory);
+            }
+        }
+
+
+        private void OpenShopMenu()
+        {
+
+        }
+
+        public bool Sell(Player _player, int shopIndex, int playerIndex)
+        {
+            return _player.Buy(Player._inventory[shopIndex], playerIndex);
+        }
+
         //Run the game
         public void Run()
         {
-            
+            Start();
+            Update();
         }
 
         //Performed once when the game begins
@@ -21,7 +109,7 @@ namespace HelloWorld
         //Repeated until the game ends
         public void Update()
         {
-            
+            OpenShopMenu();
         }
 
         //Performed once when the game ends
---
+++ Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Player
+    {
+        //private string _name;
+        private int _health;
+        private int _baseDamage;
+        private Items[] _inventory;
+        private Items _currentWeapon;
+        private Items _hands;
+        private int _gold;
+
+        public Player()
+        {
+            _health = 100;
+            _baseDamage = 5;
+            _inventory = new Items[3];
+            _hands.itemBoost = 0;
+            _hands.itemName = "Palm";
+            _gold = 10;
+        }
+
+        public Player(/*string nameVal,*/int healthVal, int damageVal, int inventorySize)
+        {
+            //_name = nameVal;
+            _health = healthVal;
+            _baseDamage = damageVal;
+            _inventory = new Items[inventorySize];
+            _hands.itemName = "Palm";
+            _hands.itemBoost = 0;
+        }
+
+        public void AddItemToInventory(Items item, int index)
+        {
+            _inventory[index] = item;
+
+        }
+
+        public Items[] GetInventory()
+        {
+            return _inventory;
+        }
+
+        public bool Buy(Items, int)
+        {
+            Items[] inventory = { };
+            
+        }
+
+
+
+        //Unneeded
+        //public string GetName()
+        //{
+        //    return _name;
+        //}
+
+
+    }
+}
